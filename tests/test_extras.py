5dfb82bbeb82ac5f5925dd854948fabd2bf2d8bf
Patrick Garrett <86084767+pgarrett-scripps@users.noreply.github.com>
2025-06-06 17:27:53 +0000

    added streamlit-notify

--- tests/test_extras.py
+++ tests/test_extras.py
@@ -14,7 +14,6 @@ def get_extras() -> List[str]:
         if extra.ispkg
     ]
 
-
 @pytest.mark.parametrize("extra", get_extras())
 def test_extra_attributes(extra: str):
     mod = import_module(f"streamlit_extras.{extra}")
@@ -23,18 +22,24 @@ def test_extra_attributes(extra: str):
     assert isinstance(mod.__desc__, str)
     assert hasattr(mod, "__funcs__")
     assert isinstance(mod.__funcs__, list)
-    for func in mod.__funcs__:
-        assert callable(func)
-    # If you have multiple functions, then each example function must
-    # specify which of these functions must be imported
-    if len(mod.__funcs__) > 1:
-        assert isinstance(mod.__examples__, dict)
-    assert len(mod.__examples__) > 0
-    if isinstance(mod.__examples__, dict):
-        for example, funcs in mod.__examples__.items():
-            assert callable(example)
-            for func in funcs:
-                assert callable(func)
+
+    is_namespace_package = getattr(mod, "__is_namespace__", False)
+    assert isinstance(is_namespace_package, bool)
+
+    if is_namespace_package == False:
+        for func in mod.__funcs__:
+            assert callable(func)
+        # If you have multiple functions, then each example function must
+        # specify which of these functions must be imported
+        if len(mod.__funcs__) > 1:
+            assert isinstance(mod.__examples__, dict)
+        assert len(mod.__examples__) > 0
+        if isinstance(mod.__examples__, dict):
+            for example, funcs in mod.__examples__.items():
+                assert callable(example)
+                for func in funcs:
+                    assert callable(func)
+
     assert isinstance(mod.__author__, str)
     if hasattr(mod, "__inputs__"):
         assert isinstance(mod.__inputs__, dict)
